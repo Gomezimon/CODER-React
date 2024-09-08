f1dce1910466dd285aae9599b66dbba3a2cf3658
simon gomez <gomezimon.ernesto@gmail.com>
2024-09-07 20:17:41 -0400

    error map corregido

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,13 +2,13 @@ import './ItemList.css'
 import Item from '../Item/Item'
 
 
-const ItemList = ({ products = [] }) => {  // Asignamos un valor por defecto
+const ItemList = ({ products = [] }) => {
     return(
         <div className='ListGroup'>
             {products.length > 0 ? (
                 products.map(prod => <Item key={prod.id} {...prod} />)
             ) : (
-                <p>No hay productos disponibles</p>  // Mensaje alternativo si no hay productos
+                <p>No hay productos disponibles</p> 
             )}
         </div>
     )

f16f902ef53d717b7fcfedeed123b6cc90c83285
simon gomez <gomezimon.ernesto@gmail.com>
2024-10-03 18:32:52 -0300

    Estilos

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import './Cart.css'
 import { useContext, useState } from "react";
 import { CartContext } from '../../context/CartContext';
 import CartItem from '../CartItem/CartItem';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { collection, getFirestore,addDoc } from 'firebase/firestore';
 
 const Cart = () => {
@@ -11,9 +11,10 @@ const Cart = () => {
         email: ""
     });
 
-    const [error, setError] = useState({});
+    const [error, setError] = useState({})
 
-    const { cart, clearCart, totalQuantity, total } = useContext(CartContext);
+    const { cart, clearCart, totalQuantity, total } = useContext(CartContext)
+    const navigate = useNavigate ()
 
     const handleChange = (e) => {
         setBuyer({
@@ -23,21 +24,21 @@ const Cart = () => {
     };
 
     const submit = (e) => {
-        e.preventDefault();
-        const localError = {};
+        e.preventDefault()
+        const localError = {}
 
         if (!buyer.name) {
-            localError.name = "El nombre es obligatorio";
+            localError.name = "El nombre es obligatorio"
         }
 
         if (!buyer.email) {
-            localError.email = "El email es obligatorio";
+            localError.email = "El email es obligatorio"
         }
 
         if (Object.keys(localError).length === 0) {
             addToCart()
         } else {
-            setError(localError);
+            setError(localError)
         }
     }
     const addToCart = () => {
@@ -51,7 +52,12 @@ const Cart = () => {
         }
 
         addDoc(orderCollection, purchase)
-        .then(res => console.log(res.id))
+        .then((res) => {
+            navigate('/checkout', {state: { orderId: res.id}})
+        })
+        .catch((error) => {
+            console.error("No se pudo crear la orden")
+        })
     }
 
     if (totalQuantity === 0) {
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,12 +1,21 @@
-
+import './Checkout.css'
+import { useLocation, Link } from 'react-router-dom'
 
 const Checkout = () => {
+    const location = useLocation()
+    const { orderId } = location.state || {}
 
-    return(
+    return (
         <div>
-            <h1>checkout</h1>
+            <h1>¡Gracias por tu compra!</h1>
+            {orderId ? (
+                <p>Tu ID de orden es: <strong>{orderId}</strong></p>
+            ) : (
+                <p>No se encontró el ID de la orden.</p>
+            )}
+            <Link to='/'>Volver al inicio</Link>
         </div>
-    )
-}
+    );
+};
 
 export default Checkout;

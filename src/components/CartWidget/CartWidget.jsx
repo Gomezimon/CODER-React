0654b567259b74f87934a60bfb0b0e477fbf6126
simon gomez <gomezimon.ernesto@gmail.com>
2024-10-08 21:38:09 -0300

    estilos y queries

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,16 +4,17 @@ import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 
-
 const CartWidget = () => {
-    const {totalQuantity} = useContext(CartContext)
+    const { totalQuantity } = useContext(CartContext);
 
     return (
-        <Link to= '/cart' className="CartWidget">
-            <img className="CartImg" src= {carritoImg} alt="cart-widget" />
-            {totalQuantity} 
+        <Link to='/cart' className="CartWidget">
+            <div className="CartIconContainer">
+                <img className="CartImg" src={carritoImg} alt="cart-widget" />
+                {totalQuantity > 0 && <span className="CartBadge">{totalQuantity}</span>}
+            </div>
         </Link>
-    )
+    );
 }
 
 export default CartWidget;
